58c6443971507918c1cf533734c5f2842bf658e7
wildcattat <tatjana.dzambazova@gmail.com>
2016-09-15 21:23:01 -0700

    imagemaps
    konami and added vars for image maps

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 
-// var kkeys = [];
-// var konami = "38,38,40,40,37,39,37,39,66,65";
+var kkeys = [];
+var konami = "38,38,40,40,37,39,37,39,66,65";
 
-// $(document).keydown(function(e) {
-//   kkeys.push( e.keyCode );
-//   if ( kkeys.toString().indexOf( konami ) >= 0 ){
-//     $(document).unbind('keydown',arguments.callee);
+$(document).keydown(function(e) {
+  kkeys.push( e.keyCode );
+  if ( kkeys.toString().indexOf( konami ) >= 0 ){
+    $(document).unbind('keydown',arguments.callee);
     
-//     // Add your own easter egg here!!
+    // Add your own easter egg here!!
+    // console.log('this works')
+    setInterval(function () {
+        $('.Boss').addClass('Bossjumping');
+    }, 300);
 
-// setInterval(function () {
-//     $('.Boss').toggleClass('Bossjumping');
-// }, 3000);
-
-//   }
-// });
+}
+});
 
 
 // make all images and menus i need invisible so they can Fadein later. ( i tried opacity, it doesnt work on all use cases)
-$('.filters').hide();
+// $('.filters').hide();
 $('#year1965').hide();
 $('#year2016').hide();
 $('.TanjaImage').fadeIn(1000);
 $('a.resumebutton').hide();
-$('.Boss').hide();
+// $('.Boss').hide();
 
 
 
@@ -55,11 +55,12 @@ $("#js-rotating").Morphext({
 // on click of the nav tabs, different timeline images should appear and the previous image should dissapear
 
 $('li.green').on("click", function (){
-    $('.MainWrap').find('img').hide();  
+    $('.MainWrap').find('img').hide();
+    // the line below is to avoid th eslider showing
+    $('.MainWrap').show();  
     $('#Geo').show();
     // to minimize the nav bar on click
     $('.filters').addClass('navMin');
-    // $('h1').hide();
     $('p.resumebutton').hide();
 
 
@@ -68,10 +69,11 @@ $('li.green').on("click", function (){
 $('li.lightgreen').on("click", function (){
     // With the line below i want to delete any previous image - so i want a syntax that says hide any existing image (any siblings) so to put a new one in th enext line
     $('.MainWrap').find('img').hide();
+    $('.MainWrap').show(); 
     $('#People').show();
     // minimize the nav bar
     $('.filters').addClass('navMin');
-    // $('h1').hide();
+    
 
 
 });
@@ -79,6 +81,7 @@ $('li.lightgreen').on("click", function (){
 $('li.darkgreen').on("click", function (){
     // With the line below i want to delete any previous image - so i want a syntax that says hide any existing image (any siblings) so to put a new one in th enext line
     $('.MainWrap').find('img').hide();
+    $('.MainWrap').show(); 
     $('#Work').show();
     // minimize the nav bar
     $('.filters').addClass('navMin');
@@ -87,18 +90,60 @@ $('li.darkgreen').on("click", function (){
 
 });
 
+$('li.orange').on("click", function (){
+    // With the line below i want to delete any previous image - so i want a syntax that says hide any existing image (any siblings) so to put a new one in th enext line
+    $('.MainWrap').find('img').hide();
+    $('.MainWrap').show(); 
+    $('#ArtsCrafts').show();
+    // minimize the nav bar
+    $('.filters').addClass('navMin');
+    // $('h1').hide();
+    $('a.resumebutton').show();
+
+});
+
+$('.imagewrap a').on("click", function (e){
+    e.preventDefault();
+    var info = $(this).attr('data-info');
+    var context = geographyInfo[info];
+    // $('.Skopje').addClass('expandedSkopje');
+
+
+    var source   = $("#modal-template").html();
+    var template = Handlebars.compile(source);
+    var htmlToInsert    = template(context);
+
+    $('.modal').html(htmlToInsert);
+
+    $('.modal').fadeIn();
+});
+
 // the above will repeat from all Timeline filters
 
 // the below is to make the nav menu minimize and maximize on hover.  it works but i have to figure out how to make it maximize only when hovering over the small minimized portion of it as now it maximisez when i am anywhere in teh proximity which is not what i want
 
+$('.close').on('mouseenter', function(){
+    $('#modal-template').hide();
+    // $('.MainWrap').addClass('opacity50');
+});
 
 $('.filters li').on('mouseenter', function(){
     $('.filters').removeClass('navMin');
     // $('.MainWrap').addClass('opacity50');
 });
 
-// $('.filters').hover(function(){
-//     $('.filters').removeClass('navMin');
-// });
 
+var geographyInfo = {
+    skopje: {
+        src: 'img/Skopje.jpg',
+        text: 'Skopje used to be a pretty subtle quiet lovely city..Until the Gang of bandits took over. As a result of their stupidity, ignorance, greed and unlimited criminal minds...'
+    },
+    people: {
+        src: 'img/Skopje.jpg',
+        text: 'Skopje used to be a pretty subtle quiet lovely city..Until the Gang of bandits took over. As a result of their stupidity, ignorance, greed and unlimited criminal minds...'
+    }
+}
+
+
+console.log(geographyInfo.people);
 // this will be the section of Image maps - when click on different image maps in the images, they should open separately and i need an X sign to be able to minimize them. have =nt yet started that
